2cdc1dc2298dca6eb0f9894c871452943849c4ed
Tejkumar Kempaiah <Tejkumar.kempaiah@csgi.com>
2022-06-26 20:11:30 +0530

    Updated script.js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 console.log('Javascript file loaded... ')
 
-// function colorChange() {
-//     window.alert('Color change logic is not implemented yet...')
-// }
-
 function refresh() {
     window.resizeBy(100, 500)
 }
