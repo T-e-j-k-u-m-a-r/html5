e2d2ce1e69e6c99e612dc8f2f3754e6f3d761109
Tejkumar Kempaiah <Tejkumar.kempaiah@csgi.com>
2022-06-26 18:15:29 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('Javascript file loaded... ')
+
+// function colorChange() {
+//     window.alert('Color change logic is not implemented yet...')
+// }
+
+function refresh() {
+    window.resizeBy(100, 500)
+}
